15404225632e023436560f1ae580437434074302
NurbekIsagaliev <71828298+NurbekIsagaliev@users.noreply.github.com>
2022-02-25 17:53:46 +0500

    Update fibbonacci function.php

--- fibbonacci function.php
+++ fibbonacci function.php
@@ -3,7 +3,12 @@
 iborow(int $limit) : string которая будет возвращать элементы последовательности 
 Фибоначчи ограниченной переданным на вход значением $limit в виде строки 
 (разделитель - пробел). Например: при входном значении 10 функция должна 
-вернуть строку "0 1 1 2 3 5 8" */
+вернуть строку "0 1 1 2 3 5 8" 
+Fibonacci sequence. Need to write a function
+iborow(int $limit) : string that will return the elements of the sequence
+Fibonacci limited by the $limit value passed to the input as a string
+(separator - space). For example: with an input value of 10, the function should
+return string "0 1 1 2 3 5 8"*/
 
 function fiborow(int $limit):string{
 $f0=1;$f1=1; $str=""; 
